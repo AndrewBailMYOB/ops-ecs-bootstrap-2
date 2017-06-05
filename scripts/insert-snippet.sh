0e73653a87e1e37c1012a4f1b35c6b41f932615d
Mel Boyce <weird@syngin.net>
2017-06-06 09:13:38 +1000

    mend for static check

--- scripts/insert-snippet.sh
+++ scripts/insert-snippet.sh
@@ -5,12 +5,12 @@ repl="${1:-repl.txt}"
 token="{replaceme}"
 
 while IFS= read -r line; do
-    if [[ "$line" =~ "$token" ]]; then
+    if [[ "$line" =~ $token ]]; then
         indent=$(echo "$line"|awk '{match($0, /^ */);print RLENGTH}')
         while IFS= read -r rline; do
-            iline=$(printf "%*s%s\n" $indent '' "$rline")
+            iline=$(printf "%*s%s\n" "$indent" '' "$rline")
             echo "$iline"
-        done <$repl
+        done <"$repl"
     else
         echo "$line"
     fi
