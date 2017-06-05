66e64e7f375a55bb6fa6c24ec7e13eaca7e309c6
Mel Boyce <weird@syngin.net>
2017-06-06 09:05:55 +1000

    initial import

---
+++ scripts/insert-snippet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+orig="/dev/stdin"
+repl="${1:-repl.txt}"
+token="{replaceme}"
+
+while IFS= read -r line; do
+    if [[ "$line" =~ "$token" ]]; then
+        indent=$(echo "$line"|awk '{match($0, /^ */);print RLENGTH}')
+        while IFS= read -r rline; do
+            iline=$(printf "%*s%s\n" $indent '' "$rline")
+            echo "$iline"
+        done <$repl
+    else
+        echo "$line"
+    fi
+done <$orig
